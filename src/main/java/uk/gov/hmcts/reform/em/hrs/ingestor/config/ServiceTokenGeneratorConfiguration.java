6ce4396753eb79c31018fe306c7fb50373b494a0
mdayican <mustafa.dayican@hmcts.net>
2023-03-08 13:44:44 +0000

    EM-5120 EnableFeignClients

--- src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/config/ServiceTokenGeneratorConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.em.hrs.ingestor.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
@@ -9,7 +8,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
-@EnableFeignClients
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
