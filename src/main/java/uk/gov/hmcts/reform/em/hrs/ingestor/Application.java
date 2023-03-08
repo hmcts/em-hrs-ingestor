a2e38b9c354425c2e367f00813f1d6ff018fe2e9
mdayican <mustafa.dayican@hmcts.net>
2023-03-08 14:05:02 +0000

    EM-5120 scan for telemetry client

--- src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/Application.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.em.hrs.ingestor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "uk.gov.hmcts.reform")
+@ComponentScan(basePackages = "uk.gov.hmcts.reform")
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
     public static void main(String[] args) {
