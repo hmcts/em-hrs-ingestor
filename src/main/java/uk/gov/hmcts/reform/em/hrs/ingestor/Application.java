50813c6fa0aa71bafc2a5c03b26cdb338277faa6
mdayican <mustafa.dayican@hmcts.net>
2023-03-08 14:11:14 +0000

    EM-5120 from feignscan

--- src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/ingestor/Application.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "uk.gov.hmcts.reform")
 @ComponentScan(basePackages = "uk.gov.hmcts.reform")
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
