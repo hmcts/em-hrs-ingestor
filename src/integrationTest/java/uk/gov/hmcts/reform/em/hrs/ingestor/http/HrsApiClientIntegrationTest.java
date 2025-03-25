4e2c631bc79a511f5cf483250c5fff68198a98a5
mdayican <mdayican@gmail.com>
2025-03-25 19:58:41 +0000

    fix tests

--- src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/ingestor/http/HrsApiClientIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/ingestor/http/HrsApiClientIntegrationTest.java
@@ -12,6 +12,8 @@ import uk.gov.hmcts.reform.em.hrs.ingestor.config.AppConfig;
 import uk.gov.hmcts.reform.em.hrs.ingestor.config.TestOkHttpClientConfig;
 import uk.gov.hmcts.reform.em.hrs.ingestor.exception.HrsApiException;
 import uk.gov.hmcts.reform.em.hrs.ingestor.http.mock.WireMockInitializer;
+import uk.gov.hmcts.reform.em.hrs.ingestor.idam.cache.IdamCacheExpiry;
+import uk.gov.hmcts.reform.em.hrs.ingestor.idam.cache.IdamCachedClient;
 import uk.gov.hmcts.reform.em.hrs.ingestor.model.HearingSource;
 import uk.gov.hmcts.reform.em.hrs.ingestor.model.HrsFileSet;
 import uk.gov.hmcts.reform.em.hrs.ingestor.model.Metadata;
@@ -35,7 +37,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.em.hrs.ingestor.helper.TestUtil.convertObjectToJsonString;
 
 @SpringBootTest(classes = {TestOkHttpClientConfig.class, AppConfig.class, HrsApiClientImpl.class,
-    HrsApiTokenService.class, IdamClient.class, IdamApi.class})
+    HrsApiTokenService.class, IdamCachedClient.class, IdamCacheExpiry.class, IdamClient.class, IdamApi.class})
 @ContextConfiguration(initializers = {WireMockInitializer.class})
 class HrsApiClientIntegrationTest {
     private static final String TEST_FILE = "file.mp4";
